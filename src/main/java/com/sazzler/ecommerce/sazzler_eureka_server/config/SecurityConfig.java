ca4b509715b3b24f834700e77f207eefdd9434df
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-09-01 16:52:34 +0530

    integrated the service registry,api gateway and the product service

---
+++ src/main/java/com/sazzler/ecommerce/sazzler_eureka_server/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.sazzler.ecommerce.sazzler_eureka_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable); // Disable CSRF for all requests
+        return http.build();
+    }
+}
